1b2ab0fe723f40f4128c39e1471d9f461212eed9
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-22 18:06:02 -0600

    carousel working now

--- public/client.js
+++ public/client.js
@@ -13,7 +13,7 @@ angular.module('bfmApp', ['ui.router'])
 				templateUrl: 'templates/vendors.html'
 			})
 			.state('vendorApply', {
-				url: '/apply',
+				url: '/vendor-application',
 				templateUrl: 'templates/vendor-application.html'
 			});
 			$locationProvider.html5Mode({
